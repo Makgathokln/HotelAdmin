83dc1dac09ad7b1d9c81a8f71ba7f4fc0df099ca
Makgathokln <51122158+Makgathokln@users.noreply.github.com>
2022-01-14 14:09:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,64 +14,71 @@ import Admin from './components/admin';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-    return (
+// function App() {
+//     return (
 
-        <
-        BrowserRouter >
-        <
-        div className = "App" >
-        <
-        Welcome / >
-        <
-        Switch >
-        <
-        Route path = "/"
-        element = { < Home / > } >
+//         <BrowserRouter>
+//         <div className = "App" >
+//         <Welcome/>
+//         <Switch>
+//         <Route path = "/"
+//         element = { < Home />}>
 
-        <
-        /Route>
+//         </Route>
 
-        <
-        Route path = "/bookings"
-        element = { < Bookings / > } >
+//         <Route path = "/bookings"
+//         element = { < Bookings />}>
 
-        <
-        /Route>
+//         </Route>
 
-        <
-        Route path = "/guests"
-        element = { < Guests / > } >
+//         <Route path = "/guests"
+//         element = { < Guests />}>
 
-        <
-        /Route>
+//         </Route>
 
-        <
-        Route path = "/rooms"
-        element = { < Rooms / > } >
+//         <Route path = "/rooms"
+//         element = { < Rooms /> }>
 
-        <
-        /Route>
+//         </Route>
 
-        <
-        Route path = "/notifications"
-        element = { < Notifications / > } >
-        <
-        /Route>
+//         <Route path = "/notifications"
+//         element = { < Notifications /> }>
+//         </Route>
 
-        <
-        Route path = "/admin"
-        element = { < Admin / > } >
+//         <Route path = "/admin"
+//         element = { < Admin /> }>
 
-        <
-        /Route>
+//         </Route>
 
-        <
-        /Switch> <
-        /div> <
-        /BrowserRouter> 
+//         </Switch> 
+//         </div> 
+//         </BrowserRouter> 
 
-    )
-}
+//     )
+// }
 
-export default App
+// export default App
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SignIn from './components/signIn';
+import SignUp from './components/signUp';
+import Welcome from './components/welcome';
+import Notifications from './components/notifications';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render( 
+    <React.StrictMode>
+    <Notifications/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(); 
+
+
